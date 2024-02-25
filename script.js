3b6cccc6ddf8fee6e4a5ace55515fa0729ad71a4
rohanmahajan03 <143026905+rohanmahajan03@users.noreply.github.com>
2024-02-24 22:28:03 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+var audio = new Audio('pigeon-sound.mp3');
+
 function countdown(isWorking){
     
     // format is 1000 * seconds * minutes + 1000
     if(isWorking){
-        var future = Date.now() + 1000 * 10 + 1000;
+        var future = Date.now() + 1000 * 3 + 1000;
     }
     else{
         var future = Date.now() + 1000 * 5 + 1000
@@ -25,7 +27,10 @@ async function displayRemainingTime(future, intervalTimeLeft, isWorking){
     console.log("entering displayRemainingTime");
     if(timeLeft <= 0){
         clearInterval(intervalTimeLeft);
+        logo.style.display = "none";
+        bottomRightImage.style.display = "none";
         console.log(isWorking);
+        audio.play();
         if(isWorking){
             document.getElementById("timer").innerHTML = "Break Time!";
             await sleep(2000);
@@ -37,30 +42,37 @@ async function displayRemainingTime(future, intervalTimeLeft, isWorking){
             document.getElementById("timer").innerHTML = "Get Back To Work!";
             await sleep(2000);
             isWorking = !isWorking;
+            // logo.style.display = "inline";
+            // bottomRightImage.style.display = "inline";
             countdown(isWorking);
         }
         
     }
     else{
-        
+        logo.style.display = "inline";
+        bottomRightImage.style.display = "inline";
         let minutesRemaining = Math.floor(timeLeft / (1000 * 60));
         let secondsRemaining = Math.floor((timeLeft - (1000 * 60 * minutesRemaining)) / 1000);
         if(isWorking){
             if(secondsRemaining < 10){
                 console.log("less than 10");
-                document.getElementById("timer").innerHTML = "Still working for " + minutesRemaining + ":0" + secondsRemaining;
+                document.getElementById("description").innerHTML = "Keep working for";
+                document.getElementById("timer").innerHTML = minutesRemaining + ":0" + secondsRemaining;
             }
             else{
-                document.getElementById("timer").innerHTML = "Still working for " + minutesRemaining +  ":"  + secondsRemaining;
+                document.getElementById("description").innerHTML = "Keep working for";
+                document.getElementById("timer").innerHTML = minutesRemaining +  ":"  + secondsRemaining;
             }
         }
         else{
             if(secondsRemaining < 10){
                 console.log("less than 10");
-                document.getElementById("timer").innerHTML = "On break for " + minutesRemaining + ":0" + secondsRemaining;
+                document.getElementById("description").innerHTML = "On break for";
+                document.getElementById("timer").innerHTML = minutesRemaining + ":0" + secondsRemaining;
             }
             else{
-                document.getElementById("timer").innerHTML = "On break for " + minutesRemaining +  ":"  + secondsRemaining;
+                document.getElementById("description").innerHTML = "Keep working for";
+                document.getElementById("timer").innerHTML = minutesRemaining +  ":"  + secondsRemaining;
             }
         }
         
@@ -69,17 +81,40 @@ async function displayRemainingTime(future, intervalTimeLeft, isWorking){
 
 }
 
-
+var intervalTimeLeft;
 var isWorking = true;
 
+var logo = document.getElementById('logo');
+var bottomRightImage = document.getElementById('bentancur');
+
+audio.play();
+
 document.getElementById('startButton').addEventListener('click', function() {
-    if(document.getElementById('startButton').innerHTML == 'Start Study Session'){
+    if(document.getElementById('startButton').innerHTML == 'Start Studying!'){
         document.getElementById('startButton').innerHTML = 'End Study Session';
         countdown(isWorking);
+        var buttonFinished = document.getElementById('startButton');
+        buttonFinished.style.display = 'none'
+        // document.getElementById('description').innerHTML = "*Close extention to stop studying*";
+
+        var pombo = document.getElementById('pombo');
+        var sonny = document.getElementById('sonny');
+        pombo.style.display = 'inline';
+        sonny.style.display = 'inline';
+        
+        logo = document.getElementById('logo');
+        logo.style.display = 'inline';
+
+        bottomRightImage = document.getElementById("bentancur");
+        bottomRightImage.style.display = 'inline';
+
     }
     else{
-        document.getElementById('startButton').innerHTML = 'Start Study Session'
         
+        
+        
+        // document.getElementById('startButton').innerHTML = 'Start Study Session';
+
     }
         
 3});
